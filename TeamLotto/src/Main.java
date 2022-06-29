5fb4cddd9879484e4bccb596454b798f4468d847
Administrator <Administrator@SC-202106161522>
2022-06-29 11:36:34 +0900

    처음부터 시작~

--- TeamLotto/src/Main.java
+++ TeamLotto/src/Main.java
@@ -2,19 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("오늘 점심 도연정 FIXED");
-<<<<<<< HEAD
-		
-		// 나도 하고싶단말이야~
-=======
-		System.out.println("ddddd");
-		System.out.println("ddddd");
-		System.out.println("ddddd");
-		// 도연정은 내가 가져간다 -- 괴도키드
-		
-		// 오늘은 뭘먹지??
-		// 안된다
->>>>>>> branch 'master' of https://github.com/junhyeokkk/Lotto.git
+		System.out.println("싹 다 지웠습니다");	// 최장호
 	}
 
 }
