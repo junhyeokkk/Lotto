c23eb082486e873d094e13d1116b24174192ae18
Administrator <Administrator@SC-202106161522>
2022-06-29 11:25:41 +0900

    뭐먹냥

--- TeamLotto/src/Main.java
+++ TeamLotto/src/Main.java
@@ -11,6 +11,8 @@ public class Main {
 		System.out.println("ddddd");
 		System.out.println("ddddd");
 		// 도연정은 내가 가져간다 -- 괴도키드
+		
+		// 오늘은 뭘먹지??
 >>>>>>> branch 'master' of https://github.com/junhyeokkk/Lotto.git
 	}
 
