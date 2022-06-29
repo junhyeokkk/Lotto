e475969ef6b50c13b597d7149dbc508d32dcbe7a
Administrator <Administrator@SC-202106161522>
2022-06-29 11:23:05 +0900

    ㅎㅎㅎㅎ

--- TeamLotto/src/Main.java
+++ TeamLotto/src/Main.java
@@ -10,8 +10,7 @@ public class Main {
 		System.out.println("ddddd");
 		System.out.println("ddddd");
 		System.out.println("ddddd");
-		System.out.println("점심 도연정 ㄱ?");
-		System.out.println("ㄴㄴ 절대 ㄴㄴ 도믿걸");
+		// 도연정은 내가 가져간다 -- 괴도키드
 >>>>>>> branch 'master' of https://github.com/junhyeokkk/Lotto.git
 	}
 
